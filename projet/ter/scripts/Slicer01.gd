8e3cefe603891fc28fe3989daaec9654e8f03780
Rashnain <51191602+Rashnain@users.noreply.github.com>
2026-02-09 16:50:38 +0100

    coupure récursive à partir de point aléatoires

--- projet/ter/scripts/Slicer01.gd
+++ projet/ter/scripts/Slicer01.gd
@@ -1,12 +1,23 @@
 extends Node3D
 
-func slice_object(target: RigidBody3D):
-	#On récupère le mesh
-	var mesh_instance = target.get_node("MeshInstance3D")
-	var old_mesh = mesh_instance.mesh
-	#on mets ses infos (triangles) dans un Array
-	var array_mesh = ArrayMesh.new()
-	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, old_mesh.get_mesh_arrays())
+var bounding_box: AABB
+var voronoi_points: PackedVector3Array
+@onready var points_3d: Node3D = $points
+@onready var base_point: MeshInstance3D = $points/MeshInstance3D
+@onready var aabb: MeshInstance3D = $points/aabb
+
+func slice_object(mesh_instance: MeshInstance3D, rec: int):
+	#await get_tree().create_timer(0.25).timeout
+	if rec == 0:
+		return
+	var array_mesh: ArrayMesh
+	if mesh_instance.mesh is PrimitiveMesh:
+		var old_mesh: PrimitiveMesh = mesh_instance.mesh
+		#on mets ses infos (triangles) dans un Array
+		array_mesh = ArrayMesh.new()
+		array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, old_mesh.get_mesh_arrays())
+	elif mesh_instance.mesh is ArrayMesh:
+		array_mesh = mesh_instance.mesh
 	#Initialisation des outils qui tracent les triangles
 	var st_left = SurfaceTool.new()
 	var st_right = SurfaceTool.new()
@@ -17,7 +28,9 @@ func slice_object(target: RigidBody3D):
 	var mdt = MeshDataTool.new()
 	mdt.create_from_surface(array_mesh, 0)
 	#le plan de coupe
-	var plane = Plane(Vector3.UP, 0)
+	#var plane_normal = Vector3(1, 1, 1)
+	var plane_normal = plane_from_points()
+	var plane = Plane(plane_normal.normalized())
 	#on parcourt les triangles du mesh
 	for i in range(mdt.get_face_count()):
 		#on récupère les sommets du triangles actuel
@@ -43,10 +56,13 @@ func slice_object(target: RigidBody3D):
 	var mesh_left = st_left.commit()
 	var mesh_right = st_right.commit()
 	
-	create_piece(mesh_left, target.global_transform, target.linear_velocity, Vector3(0, 0.2, 0))
-	create_piece(mesh_right, target.global_transform, target.linear_velocity, Vector3(0, -0.2, 0))
+	var mi3d_left := create_piece(mesh_left, mesh_instance.global_transform, mesh_instance.get_parent().linear_velocity, Vector3(-0.5, 0, 0), true)
+	var mi3d_right := create_piece(mesh_right, mesh_instance.global_transform, mesh_instance.get_parent().linear_velocity, Vector3(0.5, 0, 0), false)
 	
-	target.queue_free()
+	mesh_instance.get_parent().queue_free()
+
+	slice_object(mi3d_left, rec - 1)
+	slice_object(mi3d_right, rec - 1)
 
 func add_poly_to_st(st: SurfaceTool, poly: PackedVector3Array):
 	for i in range(1, poly.size() - 1):
@@ -54,7 +70,7 @@ func add_poly_to_st(st: SurfaceTool, poly: PackedVector3Array):
 		st.add_vertex(poly[i])
 		st.add_vertex(poly[i+1])
 
-func create_piece(m: Mesh, t: Transform3D, v: Vector3, offset: Vector3):
+func create_piece(m: Mesh, t: Transform3D, v: Vector3, offset: Vector3, is_left: bool) -> MeshInstance3D:
 	if m.get_surface_count() == 0:
 		return
 
@@ -64,17 +80,56 @@ func create_piece(m: Mesh, t: Transform3D, v: Vector3, offset: Vector3):
 	
 	new_mesh_inst.mesh = m #on lui donne le mesh qu'on vient de créer
 	new_shape.shape = m.create_convex_shape()	#on créé la boite de collision qui match la forme
-	
+	new_mesh_inst.material_override = StandardMaterial3D.new()
+	new_mesh_inst.material_override.cull_mode = BaseMaterial3D.CULL_DISABLED
+	if (is_left):
+		new_mesh_inst.material_override.albedo_color = Color.PURPLE
+	else:
+		new_mesh_inst.material_override.albedo_color = Color.PINK
 	new_body.add_child(new_mesh_inst)
 	new_body.add_child(new_shape)
+	#new_body.gravity_scale = 0
 	
 	add_child(new_body)
 	new_body.global_transform = t	#on place le nouveau mesh la ou etait l'ancien
 	#new_body.global_translate(offset)	#on y applique un leger decalage
 	new_body.linear_velocity = v #on lui donne la vitesse initiale du cube
 	
+	return new_mesh_inst
+
+func sample_aabb(amount: int):
+	# sample random points into the aabb and remove the ones that are not in the mesh
+	var start := bounding_box.position
+	var size := bounding_box.size
+	for i in amount:
+		var x := randf()
+		var y := randf()
+		var z := randf()
+		voronoi_points.append(Vector3(start.x + size.x * x, start.y + size.y * y, start.z + size.z * z))
+
+func show_points():
+	for point in voronoi_points:
+		var new_point := base_point.duplicate()
+		new_point.position = point
+		points_3d.add_child(new_point)
+	base_point.visible = false
+
+func plane_from_points() -> Plane:
+	# pick two points and return a plane
+	print(voronoi_points)
+	var plane_normal := voronoi_points[0] - voronoi_points[1]
+	voronoi_points.remove_at(0)
+	voronoi_points.remove_at(0)
+	return Plane(plane_normal.normalized())
+
 func _ready():
-	await get_tree().create_timer(1.0).timeout
-	var target = get_node_or_null("RigidBody3D")
-	if target:
-		slice_object(target)
+	var mesh: MeshInstance3D = get_node("RigidBody3D").get_node("MeshInstance3D")
+	bounding_box = mesh.custom_aabb
+	bounding_box.abs()
+	aabb.mesh.size = bounding_box.size
+	print(bounding_box.position)
+	print(bounding_box.size)
+	aabb.position = bounding_box.position + bounding_box.size/2
+	sample_aabb(50)
+	show_points()
+	slice_object(mesh, 4)
